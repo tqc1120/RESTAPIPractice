c754a3b50b494878ec6c023c9f5b8322068e9aea
tqc1120 <77753572+tqc1120@users.noreply.github.com>
2024-06-05 22:26:06 -0400

    Use "Get" to fetch info of all the universities.

---
+++ src/main/java/com/example/demo/Entity/University.java
@@ -0,0 +1,33 @@
+package com.example.demo.Entity;
+
+import java.util.List;
+
+public class University {
+    private String name;
+    private String domain;
+    private List<String> web_pages;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public List<String> getWeb_pages() {
+        return web_pages;
+    }
+
+    public void setWeb_pages(List<String> web_pages) {
+        this.web_pages = web_pages;
+    }
+}
